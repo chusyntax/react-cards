049544183803bd6f400fd5d2b57abd99e4eb3d61
chu-young <ttheko101@gmail.com>
2021-07-18 18:17:55 +0200

    added own stylings

--- src/App.js
+++ src/App.js
@@ -3,18 +3,39 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="cardWrapper">
 
    <Card heading="Wassup"
    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
    />
-   <Card  heading="Hello"
+    <Card  heading="Hello"
    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
    />
    <Card  heading="AYooo"
    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
    />
+    <Card  heading="A Project"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   />
     </div>
+
+    <div className="cardWrapper">
+
+   <Card heading="Wassup"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   />
+    <Card  heading="Hello"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   />
+   <Card  heading="AYooo"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   />
+    <Card  heading="A Project"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   />
+    </div>
+    </>
   );
 }
 
