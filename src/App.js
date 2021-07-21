beb081c0194a5a20c5ebac4eedd656cf5e298232
chu-young <ttheko101@gmail.com>
2021-07-21 15:51:30 +0200

    added image hover effect

--- src/App.js
+++ src/App.js
@@ -8,31 +8,47 @@ function App() {
 
    <Card heading="Wassup"
    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
    />
     <Card  heading="Hello"
-   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   description="Lorem ipsum dolor sits perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
    />
    <Card  heading="AYooo"
    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
    />
     <Card  heading="A Project"
    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
    />
     </div>
 
     <div className="cardWrapper">
 
    <Card heading="Wassup"
-   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   description=" repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
    />
     <Card  heading="Hello"
-   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. udiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
    />
    <Card  heading="AYooo"
-   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
    />
     <Card  heading="A Project"
-   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum ?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
    />
     </div>
     </>
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,26 @@
 import img from './112803.jpg';
+import GitHub from './github-white.png';
+import Website from './website-white.png';
 
 function Card(props) {
     return (
       <div className="card">
   <div className="cardBody">
-      <img src={img} alt="landscape" />
+
+    <div className="imageOverlay">
+    <img className="imageOverlayImg" src={img} alt="landscape" />
+      <div className="imageOverlayDescription">
+
+        <a href={props.gitHub} target="_blank" rel="noopener noreferrer">
+        <img src={GitHub} alt="GitHub Icon" /> 
+        </a>
+
+        <a href={props.liveSite} target="_blank" rel="noopener noreferrer">
+        <img src={Website} alt="liveSite Icon" /> 
+        </a> 
+       
+      </div>
+    </div>
 
       <h2 className="cardTitle">{props.heading}</h2>
       <p className="cardDescription">{props.description}</p>
